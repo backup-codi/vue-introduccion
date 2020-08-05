9eb8ce182f56d33fa639483ff9c377fda009646c
Alberto López <engineer.lopezx2@gmail.com>
2020-08-04 19:05:32 -0500

    Computed Properties y Watchers

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ new Vue({
   data() {
     return {
       name: 'Bitcoin',
+      symbol: 'BTC',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
       changePercent: 11,
       price: 8400,
@@ -18,6 +19,17 @@ new Vue({
       ],
       showPrices: false,
     };
+  },
+  computed: {
+    title() {
+      return `${this.name} - ${this.symbol}`;
+    },
+  },
+
+  watch: {
+    showPrices(newVal, oldVal) {
+      console.log(newVal, oldVal);
+    },
   },
 
   methods: {
